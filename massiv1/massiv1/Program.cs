01e29852b6c6ce01533c20a56ca7e598663a813e
Джамал <97660596+djamal1111@users.noreply.github.com>
2023-05-09 23:58:02 +0300

    Add files via upload

---
+++ massiv1/massiv1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 5, 2, 7, 8, 3, 6, 9 };
+            List<int> odds = new List<int>();
+
+            // Используем цикл для добавления нечётных элементов в новый массив
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 != 0)
+                {
+                    odds.Add(nums[i]);
+                }
+            }
+
+            // Сортируем новый массив по возрастанию
+            odds.Sort();
+
+            // Преобразуем список в массив и выводим его на экран
+            int[] result = odds.ToArray();
+            Console.WriteLine(string.Join(", ", result)); // Выведет: 3, 5, 7, 9
+        }
+    }
+}
---
+++ massiv2/massiv2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massiv2
+{
+    internal class Program
+    {
+        static int[,] MatrixPower(int[,] matrix, int power)
+        {
+            // Шаг 1
+            int[,] result = (int[,])matrix.Clone();
+
+            // Шаг 2
+            for (int p = 1; p < power; p++)
+            {
+                int[,] temp = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+                // Шаг 2.2
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        int sum = 0;
+                        int min = int.MaxValue;
+
+                        // Вычисляем значение элемента новой матрицы
+                        for (int k = 0; k < matrix.GetLength(1); k++)
+                        {
+                            if (matrix[i, k] + matrix[k, j] < min)
+                            {
+                                min = matrix[i, k] + matrix[k, j];
+                            }
+                        }
+
+                        temp[i, j] = min;
+                    }
+                }
+
+                // Шаг 2.3
+                result = temp;
+
+                // Заменяем исходную матрицу на новую
+                matrix = (int[,])temp.Clone();
+            }
+
+            return result;
+        }
+
+    }
+}
